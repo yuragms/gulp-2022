e56a3a969d2de72596b5c17fddfd77f36290ed13
yuragms <yura.gms@gmail.com>
2022-04-17 11:49:10 +0300

    1

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,16 +7,26 @@ const srcFolder = `./src`;
 
 export const path = {
   build: {
+    js: `${buildFolder}/js/`,
+    css: `${buildFolder}/css`,
     html: `${buildFolder}/`,
+    images: `${buildFolder}/img/`,
     files: `${buildFolder}/files/`,
   },
   src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
   },
   watch: {
-    html: `${srcFolder}/**/*.html` },
-    files: `${srcFolder}/files/**/*.*`
+    js: `${srcFolder}/js/**/*.js`,
+    scss: `${srcFolder}/scss/**/*.scss`,
+    html: `${srcFolder}/**/*.html`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    files: `${srcFolder}/files/**/*.*`,
   },
   clean: buildFolder,
   buildFolder: buildFolder,
---
+++ gulp/config/plugins.js
@@ -0,0 +1,14 @@
+import replace from "gulp-replace"; // Поиск и замена
+import plumber from "gulp-plumber"; // Обработка ошибок
+import notify from "gulp-notify"; // Сообщения (подсказкиё)
+import browsersync from "browser-sync"; // Локальный сервер
+import newer from "gulp-newer"; //Проверка обновления
+// Экспортируем обьект
+
+export const plugins = {
+  replace: replace,
+  plumber: plumber,
+  notify: notify,
+  browsersync: browsersync,
+  newer: newer,
+};
